689b4f9bac0d5df8262fc962b53b40e69339323a
Sri Vishnuvardhan A <53358337+vishnuswmech@users.noreply.github.com>
2024-10-18 15:31:26 +0530

    Helm install instructions on user data

--- init.sh
+++ init.sh
@@ -31,4 +31,8 @@ echo \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
 echo "User data script completed."
